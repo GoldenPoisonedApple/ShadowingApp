d2eacd59f8ccfb35c00f92d0bc3a30ca1e15d312
ito <nanisiyou1@gmail.com>
2025-04-20 22:45:24 +0900

    上手くいかん

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,45 @@
-﻿namespace ShadowingApp.ViewModels;
+﻿using System.Threading.Tasks;
+using ReactiveUI;
+using System.Windows.Input;
+using System.Reactive.Linq;
+
+namespace ShadowingApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+	// プライベートフィールドを追加
+	private string _debugText = "Welcome to Avalonia!";
+
+	// プロパティのゲッターとセッターを定義
+	public string DebugText
+	{
+		get => _debugText;
+		private set
+		{
+			_debugText = value;
+			this.RaisePropertyChanged(nameof(DebugText));
+		}
+	}
+
+	// コマンドを宣言
+	public ICommand SelectedVoice { get; }
+
+	// コンストラクタ
+	public MainWindowViewModel()
+	{
+		// ReactiveCommandを使用してコマンドを作成
+		SelectedVoice = ReactiveCommand.CreateFromTask(async () =>
+		{
+			await TestFunc(); // コマンド実行時の処理を呼び出す
+			DebugText = "Selected Voice Executed!"; // セッターを通じて変更通知も発行
+		});
+	}
+
+	// コマンドの実行内容を定義
+	private async Task TestFunc()
+	{
+		// ここに非同期処理を記述
+		await Task.Delay(1000); // 1秒待機
+		DebugText = "TestFunc executed!"; // デバッグテキストを更新
+	}
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -4,8 +4,8 @@ namespace ShadowingApp.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
 }
